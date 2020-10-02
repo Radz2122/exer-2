a15ba39c176bfe77e8c6ed3c627f92feda713ab0
Kristian Chung <kristian_chung1@hotmail.com>
2020-10-02 11:03:18 -0400

    Changement du header de la page

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'exer-2' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu principale ex-2', 'exer-2' ); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
